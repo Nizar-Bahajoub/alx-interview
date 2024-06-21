4736c42dba4164c249ca9be74823d092850333a5
Nizar Bahajoub <n.bahajoub@gmail.com>
2024-06-21 12:13:39 +0000

    Second Try done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,13 +9,11 @@ def canUnlockAll(boxes):
     boxes_to_visit = [0]
 
     while boxes_to_visit:
-        current = boxes_to_visit.pop(0)
+        current = boxes_to_visit.pop()
         visited_boxes.add(current)
         current_keys = boxes[current]
         for key in current_keys:
-            if key not in visited_boxes:
+            if key < len(boxes) and key not in visited_boxes:
                 boxes_to_visit.append(key)
 
-    if len(visited_boxes) == len(boxes):
-        return True
-    return False
+    return len(visited_boxes) == len(boxes)
