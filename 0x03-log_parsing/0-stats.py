2835f88ae54b192f5647705756bf46c1691fab10
nizar <n.bahajoub@gmail.com>
2024-06-22 22:33:51 +0000

    Pycodestyle checked

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,28 +11,24 @@ import re
 lines_count = 0
 total_size = 0
 status_code_counts = {
-        '200' : 0,
-        '301' : 0,
-        '400' : 0,
-        '401' : 0,
-        '403' : 0,
-        '404' : 0,
-        '405' : 0,
-        '500' : 0
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
 }
 
-#The pattern that the line should follow
-"""log_pattern = r'''^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3)} - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] "GET /projects/260 HTTP/1.1" (200|301|400|401|403|404|405|500) (\d+)$'''
-pattern = re.compile(log_pattern)
-"""
 
-#Signal Hundler for (Ctrl + C)
+# Signal Hundler for (Ctrl + C)
 def interrupt_handler(sig, frame):
     print_metrics()
     sys.exit(0)
 
 
-#Print_metrics Function
+# Print_metrics Function
 def print_metrics():
     global total_size, status_code_counts
     print("Total file size: {}".format(total_size))
@@ -42,35 +38,39 @@ def print_metrics():
             print("{}: {}".format(code, status_code_counts[code]))
 
 
-#Adding Signal Hundler
+# Adding Signal Hundler
 signal.signal(signal.SIGINT, interrupt_handler)
 
 
-#Reading Input lines
+# Reading Input lines
 try:
     while True:
 
         """ Pattern check """
         line = sys.stdin.readline().strip()
-        #match = pattern.match(line)
+        # match = pattern.match(line)
 
         '''if not match:
             continue'''
+        try:
+            status_code = line.split()[-2]
+            if status_code in status_code_counts:
+                status_code_counts[status_code] += 1
+        except BaseException:
+            pass
 
-        file_size = int(line.split()[-1])
-        status_code = line.split()[-2]
+        try:
+            file_size = int(line.split()[-1])
+        except BaseException:
+            pass
 
         total_size += file_size
 
-        if status_code in status_code_counts:
-            status_code_counts[status_code] +=1
-
-        lines_count +=1
-
+        lines_count += 1
 
         if lines_count % 10 == 0:
             print_metrics()
 
 except KeyboardInterrupt:
-        print_metrics()
-        sys.exit(0)
+    print_metrics()
+    sys.exit(0)
