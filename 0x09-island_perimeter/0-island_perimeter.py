7511f77df91cae56d02e37136eca38126bc27df6
Nizar <n.bahajoub@gmail.com>
2024-12-03 00:18:04 +0000

    Task Done

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""
+Return Perimeter of the island
+"""
+def island_perimeter(grid):
+    """Calculate the perimeter of an Island"""
+    if not grid or not grid[0]:
+        return 0
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,1 @@
+Islnad Perimeter
